5782898ddcd981ac722e3dd2c4d3709305b997bb
AbhiGEU <72155772+AbhiGEU@users.noreply.github.com>
2020-10-01 13:09:52 +0530

    Update Seating_Arrangement.c

--- Seating_Arrangement.c
+++ Seating_Arrangement.c
@@ -1,61 +1,69 @@
 #include<stdio.h>
-void check(int);
-main()
+void seat(int n)
 {
-	int n,c=0,i,j,s[18][6],flg;
-	scanf("%d",&n);
-	int sn[n];
-		for(i=0;i<n;i++)
-	{
-		scanf("%d",&sn[i]);
-	}
-	
-	
-	
-	for(i=0;i<18;i++)
-	{
-		for(j=0;j<6;j++)
-		{
-			c++;
-			s[i][j]=c;
-		}
-		i++;
-		for(j=5;j>=0;j--)
-		{
-			c++;
-			s[i][j]=c;
-		}
-	}
-	
-
-for(c=0;c<n;c++)
+    int i,seat,l;
+    for(i=0;i<n;i++)
 {
-flg=0;	
+    int a=6,b=-1;
+    if(n>=(12*i+1)&&n<((12*i+1)+6))
+    {
+    for(l=(12*i)+1;l<((12*i+1)+6);l++)
+    {
+            a-=1;
+                if(l==n)
+            {
+                  seat=n+(2*a + 1);
+                  printf("%d",seat);
+                    break;
+            }
+    }
+    }
+    else if(n>=(6*(2*i+1)+1)&&n<(6*(2*i+1)+7))
+    {
+        for(l=(6*(2*i+1)+1);l<(6*(2*i+1)+7);l++)
+        {
+          b+=1;
+        if(l==n)
+           {
+                 seat=n-(2*b+1);
+                  printf("%d",seat);
+                  break;
+           }
+        }
+    }
+}
 for(i=0;i<18;i++)
+{
+     if(n==((6*i)+1)||n==((6*i+1)+5))
+                   {
+                        printf(" WS\n");
+                        break;
+                   }
+                  else if(n==((6*i)+2)||n==((6*i+1)+4))
+                          {
+                              printf(" MS\n");
+                              break;
+                          }
+                          else if(n==((6*i)+3)||n==((6*i)+4))
+                           {
+                                printf(" AS\n");
+                                break;
+                           }
+}
+}
+void main()
+{
+    int t,i,n;
+    scanf("%d",&t);
+	if(t>=1&&t<=100000)
 	{
-		for(j=0;j<6;j++)
-		{
-		 if(s[i][j]==sn[c])	
-		{
-		    flg=1;
-		    if(i%2==0)
-		    i++;
-		    else
-		    i--;
-			break;
-		}
-		}
-		if(flg==1)
-	{
-	printf("%d ",s[i][j]);
-	if(j==0||j==5)
-	printf("WS\n");
-	else if(j==1||j==4)
-	printf("MS\n");
-	else
-	printf("AS\n");
-	break;
+		for(i=0;i<t;i++)
+    {
+    scanf("%d",&n);
+	if(n>=1&&n<=108)
+    seat(n);
+	continue;
     }
 	}
-	}
+    
 }
